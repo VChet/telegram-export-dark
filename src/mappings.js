80e10b71f350092a4721b1130749304bf5c83c7b
VChet <17050347+VChet@users.noreply.github.com>
2022-09-04 20:57:26 +0500

    fix: update code block colors

--- src/mappings.js
+++ src/mappings.js
@@ -1,13 +1,15 @@
 module.exports = {
   // Background
   "$background: #fff": "var(--gray-dark)", // main bg
-  "$background: #f5f5f5": "var(--gray)", // code
+  "$background: #f5f5f5": "var(--gray-light)", // code block
+  "$background: #f9f2f4": "var(--gray-light)", // code
   // Color
   "color: #000": "color: var(--text)", // main text
   "color: #70777b": "color: var(--light)", // details
   "color: #212121": "color: var(--light)", // bold
   "color: #168acd": "color: var(--main)", // links
-  "color: #333": "color: var(--main)", // code
+  "color: #333": "color: var(--main)", // code block
+  "color: #c7254e": "color: var(--main)", // code
   // Borders
   "$border: #e3e6e8": "var(--gray-light)", // divider
   "$border: #eee": "var(--gray-light)", // code
